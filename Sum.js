666997875cc3c68e8574b4a0f12704eb51b31536
Akash <akashsuryawanshi830@gmail.com>
2022-01-21 15:57:54 +0530

    initial commit

---
+++ Sum.js
@@ -0,0 +1,7 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 10;
+var number2 = 7.5;
+var result = sum(number1, number2);
+console.log(result);
---
+++ Sum.ts
@@ -0,0 +1,10 @@
+function sum(num1:number, num2:number){
+
+    return num1+num2;
+}
+
+var number1 = 10;
+var number2 = 7.5;
+
+var result = sum(number1,number2);
+console.log(result);
